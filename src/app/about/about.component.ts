bbf3bac12b7479830aa9af25978102cc9dde8b3a
Andrés Felipe Rincón <andrew.rincon.94@gmail.com>
2024-12-06 17:05:33 -0500

    Add new about sections

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { AboutBannerComponent } from "../components/about-banner/about-banner.component";
 import { OurMissionComponent } from "../components/our-mission/our-mission.component";
+import { OurHistoryComponent } from "../components/our-history/our-history.component";
+import { OurValuesComponent } from "../components/our-values/our-values.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [AboutBannerComponent, OurMissionComponent],
+  imports: [AboutBannerComponent, OurMissionComponent, OurHistoryComponent, OurValuesComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
